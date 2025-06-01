4aabbf268f905d6b3ccce6926568e743c94daec0
Akshraj Kumar <ankitkumargola247@gmail.com>
2025-06-02 01:30:32 +0530

    Change Flowchart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -117,7 +117,7 @@ export default function Home() {
           viewport={{ once: true }}
           transition={{ duration: 1 }}
         >
-          <img src="/src/images/flowchart.png" alt="Flowchart" className="rounded-xl shadow-xl max-w-md" />
+          <img src="/images/flowchart.png" alt="Flowchart" className="rounded-xl shadow-xl max-w-md" />
           <div>
             <h2 className="text-3xl font-bold text-blue-700 mb-4">How UrticaScan AI Works</h2>
             <p className="text-gray-700 text-lg">
